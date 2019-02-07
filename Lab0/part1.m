bc080e2365b35acbefd2c493323e686354a50e4f
lianghuang <huangliang0826@icloud.com>
2019-02-07 16:42:23 +0100

    Correct some errors in part1.m

--- Lab0/part1.m
+++ Lab0/part1.m
@@ -1,6 +1,6 @@
 function [] = part1()
 % This function is a demo for dividing an image into four pieces and rearrange
-% the 4 pieces together.
+% the 4 pieces in different order.
 % No Input.
 % No Output.
 
@@ -9,7 +9,7 @@ im1 = imread('peppers.png');
 im2 = im2double(im1);
 s = size(im2);
 
-% Divede the image into 4 parts.
+% Divide the image into 4 parts.
 sub1 = im2(1:s(1)/2,1:s(2)/2,:);
 sub2 = im2(1:s(1)/2,s(2)/2+1:s(2),:);
 sub3 = im2(s(1)/2+1:s(1),1:s(2)/2,:);
@@ -20,25 +20,25 @@ figure()
 subplot(2,2,1);
 imshow(sub1)
 
-subplot(2,2,2); 
+subplot(2,2,2);
 imshow(sub2)
 
 subplot(2,2,3);
 imshow(sub3)
 
-subplot(2,2,4); 
+subplot(2,2,4);
 imshow(sub4)
 
 figure()
 subplot(2,2,2);
 imshow(sub1)
 
-subplot(2,2,1); 
+subplot(2,2,1);
 imshow(sub2)
 
 subplot(2,2,4);
 imshow(sub3)
 
-subplot(2,2,3); 
+subplot(2,2,3);
 imshow(sub4)
 end
